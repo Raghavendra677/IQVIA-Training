578bbadae5a350670968b168fa031516032a131d
Raghavendra <raghavendrary677@gmail.com>
2025-12-17 16:27:16 +0530

    day 5 programs

---
+++ src/Day6/ThrowExample.java
@@ -0,0 +1,20 @@
+package Day6;
+
+
+public class ThrowExample {
+    public static void main(String[] args) {
+        try {
+            checkAge(15);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Caught Exception:" + e.getMessage());
+        }
+    }
+
+    static void checkAge(int age) {
+        if (age < 18) {
+            throw new IllegalArgumentException("Age must be 18 or above.");
+        }
+        System.out.println("Access granted.");
+    }
+
+}
