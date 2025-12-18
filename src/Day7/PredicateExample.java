6b3411c75cfcf8eb31d494eb737b440bf32ac9aa
Raghavendra <raghavendrary677@gmail.com>
2025-12-18 20:13:58 +0530

    Day 7 Updates

---
+++ src/Day7/PredicateExample.java
@@ -0,0 +1,25 @@
+package Day7;
+
+
+import java.util.function.Predicate;
+
+class EvenCheck implements Predicate<Integer> {
+    @Override
+    public boolean test(Integer num) {
+        return num % 2 == 0;
+    }
+}
+
+public class PredicateExample{
+    public static void main(String[] args) {
+        Predicate<Integer> C = new EvenCheck();
+
+
+        if (C.test(11)) {
+            System.out.println("number is even.");
+        } else {
+            System.out.println("number is odd.");
+        }
+    }
+
+}
